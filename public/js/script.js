efdd88040159925e84be0bfbd9529ea9638ed076
Anastasia Lucas <anastasialucas@Anastasias-MacBook-Pro.local>
2018-03-05 16:30:25 -0500

    working

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,16 @@
 (function() {
-  var ctx = new (window.AudioContext || window.webkitAudioContext)();
-  //var audioElement = document.getElementById('audioElement');
-  var audio = document.createElement('audio');
-  audio.crossOrigin = "anonymous";
-  audio.src = "./audio/RunningModerat.mp3";
-  var audioSrc = ctx.createMediaElementSource(audio);
-  var analyser = ctx.createAnalyser();
-  //bind analyser to media element source
-  audioSrc.connect(analyser);
-  audioSrc.connect(ctx.destination);
-  audio.play();
 
-  //number = analyser.frequencyBinCount
-  var frequencyData = new Uint8Array(200);
+    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    var audioElement = document.getElementById('audioElement');
+    var audioSrc = audioCtx.createMediaElementSource(audioElement);
+    var analyser = audioCtx.createAnalyser();
+
+    // bind our analyser to the media element source.
+    audioSrc.connect(analyser);
+    audioSrc.connect(audioCtx.destination);
+
+    // var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+    var frequencyData = new Uint8Array(200);
 
     var svgHeight = 600,
         svgWidth = 960;
@@ -29,30 +27,32 @@
 
         // copy frequency data to frequencyData array.
         analyser.getByteFrequencyData(frequencyData);
-        // console.log(frequencyData);
+        //console.log(frequencyData);
 
         // scale things to fit
-        var radiusScale = d3.scaleLinear()
+        var radiusScale = d3.scale.linear()
             .domain([0, d3.max(frequencyData)])
             .range([0, svgHeight/2 -10]);
 
-        var hueScale = d3.scaleLinear()
+        var hueScale = d3.scale.linear()
             .domain([0, d3.max(frequencyData)])
             .range([0, 360]);
 
        // update d3 chart with new data
-      svg.selectAll('circle')
-           .data(frequencyData)
-           .enter()
-           .append('circle')
-           .attr({
+       var circles = svg.selectAll('circle')
+           .data(frequencyData);
+
+        circles.enter().append('circle');
+
+        circles
+            .attr({
                 r: function(d) { return radiusScale(d); },
                 cx: svgWidth / 2,
                 cy: svgHeight / 2,
                 fill: 'none',
                 'stroke-width': 4,
                 'stroke-opacity': 0.4,
-                stroke: function(d) { return d3.hsl(hueScale(d), 1, 0.5); }
+                stroke: function(d) { return d3.hsl(hueScale(d), 1, 0.2); }
            });
 
         circles.exit().remove();
@@ -61,7 +61,5 @@
     // run the loop
     renderChart();
 
-    // just for blocks viewer size
-    d3.select(self.frameElement).style('height', '700px');
 
 }());
